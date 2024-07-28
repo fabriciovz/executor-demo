174496a5d495b8c471ba4dea82859cd029f98fdc
Fabricio Bravo Guevara <fabribraguev@gmail.com>
2024-07-27 22:10:30 -0400

    testing invokeany with callable

--- src/main/java/com/fabribraguev/Main.java
+++ src/main/java/com/fabribraguev/Main.java
@@ -22,6 +22,21 @@ class Service implements Runnable {
     }
 }
 
+class Service2 implements Callable<String> {
+    int i;
+    public Service2(int i){
+        this.i=i;
+    }
+
+    @Override
+    public String call() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("In thread: "+i);
+        return "From thread: "+i;
+
+    }
+}
+
 /*
 ExecutorService:
 
@@ -30,7 +45,7 @@ shutDown() [*]
 awaitTermination() [*]
 Future--stores data received by submit
 submit(new Runnable()) [*] returns null
-submit(new Callable())
+submit(new Callable()) [*] returns data
 invokeAny()
 invokeAll()
  */
@@ -38,11 +53,43 @@ invokeAll()
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        submitSample();
-
+        invokeAnySample();
+        //submitCallableSample();
+        //submitRunnableSample();
         //executeSample();
     }
-    public static void submitSample() throws InterruptedException, ExecutionException {
+    public static void invokeAnySample() throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        System.out.println(new Date());
+        List<Callable<String>> futureList = new ArrayList<>();
+        futureList.add(new Service2(1));
+        futureList.add(new Service2(2));
+        futureList.add(new Service2(3));
+        futureList.add(new Service2(4));
+        futureList.add(new Service2(5));
+        String str = executorService.invokeAny(futureList);//it returns only one future data
+        //it can be any from the list of data from callable
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS); //wait till here
+        System.out.println("======================");
+        System.out.println(str);
+        System.out.println(new Date());
+    }
+    public static void submitCallableSample() throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        System.out.println(new Date());
+        List<Future<String>> futureList = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            futureList.add(executorService.submit(new Service2(i)));
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS); //wait till here
+        System.out.println("======================");
+        for(Future<String> fut:futureList)
+            System.out.println(fut.get());
+        System.out.println(new Date());
+    }
+    public static void submitRunnableSample() throws InterruptedException, ExecutionException {
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         System.out.println(new Date());
         List<Future<String>> futureList = new ArrayList<>();
@@ -55,7 +102,6 @@ public class Main {
         for(Future<String> fut:futureList)
             System.out.println(fut.get());
         System.out.println(new Date());
-
     }
     public static void executeSample(){
         ExecutorService executorService = Executors.newFixedThreadPool(5);
